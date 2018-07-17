e37f1113c173b33d79db20d195e31bebd2ed61ce
dustinglass <dustin.glass@gmail.com>
2018-07-17 16:05:03 -0300

    Update utils.py
    Reduced to necessary functions

--- utils.py
+++ utils.py
@@ -1,62 +1,10 @@
 import re
-from datetime import datetime
-import json
-import string
+from string import punctuation
 import unicodedata
 
-UNITS = {
-    'ml': ['ml'],
-    'L': ['litro'],
-    'g': ['g'],
-    'kg': ['kg'],
-    'un': ['unidades', 'unidade', 'un']
-}
-RE_UNITS = [
-    re.compile('([0-9],)*([0-9]+)\s*([A-z]+)$'),
-    re.compile('([0-9],)*([0-9]+)\s*([A-z]+)\s'),
-    re.compile('([0-9],)*([0-9]+)\s*([A-z]+)\W')
-]
-PUNCTUATION = re.compile('[%s]' % re.escape(string.punctuation))
 
-class UnitString:
-    """ Class object with a string parsed into units if applicable. """
-    def __init__(self, text):
+PUNCTUATION = re.compile('[%s]' % re.escape(punctuation))
 
-        self.match = None
-        self.value = None
-        self.units = None
-
-        text = text.lower()
-        for r in RE_UNITS:
-            match = re.search(r, text)
-            if match:
-                for k, v in UNITS.items():
-                    for u in v:
-                        if u == match.group(3):
-                            self.match = match
-                            self.units = k
-                            if match.group(1):
-                                self.value = float(
-                                    match.group(1).replace(',', '') + '.' + \
-                                    match.group(2)
-                                )
-                            else:
-                                self.value = float(match.group(2))
-                                return
-
-def time_string():
-
-    return datetime.utcnow().strftime('%Y%m%d%H%M%S%f')
-
-def write_to_json(data, file_name):
-
-    with open(file_name, 'w') as file:
-        json.dump(data, file)
-
-def read_from_json(file_name):
-
-    with open(file_name, 'r') as file:
-        return json.load(file)
 
 def truncate_whitespace(text):
     """ Returns text with unnecessary whitespace removed. """
